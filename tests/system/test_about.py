169a24dbcb45d187bc8bc781408017e533bcfc32
madabu <madalinaalbuag@gmail.com>
2023-05-19 12:00:01 +0300

    comments&about

--- tests/system/test_about.py
+++ tests/system/test_about.py
@@ -11,8 +11,20 @@ class TestAbout(TestCase):
 
     def test_about_template_render(self):
         with app.test_client() as c:
+            #sending a GET request to the /about route of app through test client
+            #and storing the response received from the server in the response variable
             response = c.get('/about')
-            expected_heading= b'<h1>About Routing and View Functions</h1>'
+
+            #assigning to a variable what we want to verify and compare to the actual response
+            #b' - string should be treated as a sequence of bytes
+            #byte strings
+            #response.data returns the content of the response as bytes
+            expected_heading = b'<h1>About Routing and View Functions</h1>'
+
+            #extracting the content of the response received from the server
+            #assigning the actual data of the response
             rendered_template = response.data
 
+            #verify that the data in expected_heading is contained within the actual data
+            #from the rendered_template
             self.assertIn(expected_heading,rendered_template)
